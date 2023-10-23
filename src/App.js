70fcadb610e7a0e3d80fc044d6d4fa37025ab08d
unknown <jawerian09@gmail.com>
2023-10-23 01:32:47 -0700

    jaweria-nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { LiaFacebookF } from "react-icons/lia";
+import { LiaGithub } from "react-icons/lia";
+import { LiaWhatsapp } from "react-icons/lia";
+import { LiaPhoneSolid } from "react-icons/lia";
+import { LiaEnvelopeOpenSolid } from "react-icons/lia";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      {/* ------------------------- */}
+      <header>
+        <div className='header'>
+          <div className='logoDiv'>
+            LOGO
+          </div>
+          <ul className='navul'>
+            <li>Home</li>
+            <li className='leftlist'>services</li>
+            <li className='leftlist'>portfolio</li>
+            <li className='leftlist'>about</li>
+            <li className='leftlist'>contact</li>
+          </ul>
+
+        </div>
+        {/* ---------------- */}
+        <nav>
+          <div className='sideDiv'>
+            <div className='icondiv'>
+              <LiaFacebookF />
+              <LiaWhatsapp />
+              <LiaGithub />
+            </div>
+          </div>
+          <div className='infodiv'>
+            <div className='info1'>
+              <LiaEnvelopeOpenSolid className='icon'/>
+              <p>info@yourwebsite.com</p>
+            </div>
+            <div className='info2'>
+              <LiaPhoneSolid className='icon'/>
+              <p>123456789</p>
+            </div>
+          </div>
+        </nav>
+
       </header>
+      <div className='banner'>
+        <div className='mainsection'>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
