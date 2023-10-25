bae5b0f8bb99d97d3e435d50ae94999996ae80cd
M-yasin4127 <148951638+M-yasin4127@users.noreply.github.com>
2023-10-25 14:29:39 -0700

    platforms

---
+++ src/components/platforms/platforms.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+ const Platforms = () => {
+  return (
+    <div className='platforms'>
+        <div className="left">
+            <div className="box1">
+                 <img src="/images/top-developer.png.webp" alt="" />
+                 <p>Top Mobile App Development Company</p>
+            </div>
+            <div className="box1">
+                 <img src="/images/good-firms.png.webp" alt="" />
+                 <p>Top Mobile App Development Company</p>
+            </div>
+            <div className="box1">
+                 <img src="/images/trust-pilot.png.webp" alt="" />
+                 <p>Top Mobile App Development Company</p>
+            </div>
+        </div>
+        <div className="center">
+            <img src="/images/mobile-1.png.webp" alt="" />
+        </div>
+        <div className="right">
+        <div className="box1 box2">
+                 <img src="/images/up-work.png.webp" alt="" />
+                 <p>Top Mobile App Development Company</p>
+            </div>
+            <div className="box1 box2">
+                 <img src="/images/developer_new.png.webp" alt="" />
+                 <p>Top Mobile App Development Company</p>
+            </div>
+            <div className="box1 box2">
+                 <img src="/images/developer_new.png" alt="" />
+                 <p>Top Mobile App Development Company</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Platforms
