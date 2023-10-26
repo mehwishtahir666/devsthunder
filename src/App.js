3aec90aa208c0ca2336a4161bf356abae31a3b98
mehwishtahir666 <mehwishtahir666@gmail.com>
2023-10-26 13:08:31 +0500

    about

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Platforms from './components/platforms/platforms';
+import About from "./components/about/about"
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/platforms" element={<Platforms/>}/>
+      <Route path="/about" element={<About/>}/>
+
     </Routes>
     </BrowserRouter>
   );
