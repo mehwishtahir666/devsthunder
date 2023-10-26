c50df8df381f3e023ae7d4da9f225093b9c7c657
mehwishtahir666 <mehwishtahir666@gmail.com>
2023-10-26 13:13:39 +0500

    componnets names

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
-import Platforms from './components/platforms/platforms';
-import About from "./components/about/about"
+import Platforms from './components/platforms/Platforms';
+import About from "./components/about/About"
 function App() {
   return (
     <BrowserRouter>
