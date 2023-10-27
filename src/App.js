f432edb74938378be05b1b8a0060f00d40d65576
mehwishtahir666 <mehwishtahir666@gmail.com>
2023-10-27 15:16:58 +0500

    projects

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ import Footer from "./components/footer/footer"
 import Header from "./components/header/header"
 import Services from "./components/services/services"
 import Trending from "./components/trending/trending"
+import Projects from './components/projects/projects';
+import Home from './components/Home';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Home/>
       <Route path="/platforms" element={<Platforms/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/banner" element={<Banner/>}/>
@@ -21,7 +24,7 @@ function App() {
       <Route path="/header" element={<Header/>}/>
       <Route path="/services" element={<Services/>}/>
       <Route path="/trending" element={<Trending/>}/>
- 
+      <Route path="/projects" element={<Projects/>}/>
     </Routes>
     </BrowserRouter>
   );
