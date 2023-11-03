77f40ed7a4a76e4a7a5fbacf5a7f945e294e107f
harisalih <147698965+harisalih@users.noreply.github.com>
2023-11-03 21:42:10 +0500

    services3

--- src/components/services3/services3.jsx
+++ src/components/services3/services3.jsx
@@ -1,14 +1,57 @@
 import React from 'react'
 import "./services3.css"
+import { BsArrowUpCircleFill,BsArrowRightShort } from "react-icons/bs";
 
-const  Services3= () => {
+const Services3 = () => {
   return (
     <>
-    <div className='services3-main'>Services3
-    ooooooooooooooigfvbn
-    </div>
-    </> 
+      <div className='services3-main'>
+        <div className='radius-box'>
+          <div className="text-box">
+            <h1>Driving Real Results</h1>
+          </div>
+          <div className='circle-main'>
+            <div className='circle-pairnt'>
+              <div className='circle-cube'>
+                <div className="circle">
+                  <div className="small-circle">
+                    <h1>3%</h1>
+                    <BsArrowUpCircleFill className='arrow-icon' />
+                  </div>
+                </div>
+                <p>Of google advertiser</p>
+              </div>
+              <div className='circle-cube'>
+              <div className="circle">
+                <div className="small-circle">
+                  <h1>100%</h1>
+                  <BsArrowUpCircleFill className='arrow-icon' />
+                </div>
+              </div>
+              <p>Growth Clients</p>
+              </div>
+              <div className='circle-cube'>
+              <div className="circle">
+                <div className="small-circle">
+                  <h1>15m</h1>
+                  <BsArrowUpCircleFill className='arrow-icon' />
+                </div>
+              </div>
+              <p>Managed budgets</p>
+              </div>
+            </div>
+            <div className='round-box'>
+              <div className='inner-box'>
+                <h1>282,000+</h1>
+                <p>Leads genrated so far </p>
+                <button>CONTACT US <BsArrowRightShort className='go'/></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default  Services3
+export default Services3
