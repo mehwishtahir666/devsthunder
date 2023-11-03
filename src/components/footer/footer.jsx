8f0618967cd144416a4ff3c58a6200937605f0e5
Jaweria Naveed <jawerian09@gmail.com>
2023-11-03 13:00:54 -0700

    final

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,10 @@ import logo from "../header/image/WhatsApp_Image_2023-10-25_at_12.09.10_8e72b0d7
 import { LiaFacebookF } from "react-icons/lia";
 import { BiLogoLinkedin } from "react-icons/bi";
 import { BiLogoTwitter } from "react-icons/bi";
-import { TiSocialSkype } from "react-icons/ti"
+import {GoLocation} from "react-icons/go"
+import { LiaEnvelopeOpenSolid } from "react-icons/lia";
+import { LiaPhoneSolid } from "react-icons/lia";
+import {LiaWhatsapp} from "react-icons/lia"
 const Footer = () => {
   return (
     <div>
@@ -12,49 +15,50 @@ const Footer = () => {
         <div className="row border-0 footer_row1 border-success w-100 m-0 ">
           {/* ----------- col1 -------------- */}
           <div className="footer_col1 col-12 col-md-6  col-xl-3 text-center border-0 border-warning m-0 p-0">
-            <div className="footer_card ">
+            <div className="footer_card">
               <div className="footer_logo m-auto">
                 <img className=" m-0 p-0" src={logo} alt="" />
               </div>
-              <h6 className="fw-bold coloryellow m-0 mt-3 p-0">TOLL-FREE</h6>
-              <p className="parasize  m-0 p-0">(855) 5-GrowMe</p>
-              <div className="footer_icons-div m-auto mt-2">
-                <div className="footer_icon"><LiaFacebookF /></div>
-                <div className="footer_icon"><BiLogoLinkedin/></div>
-                <div className="footer_icon"><BiLogoTwitter /></div>
-                <div className="footer_icon"><TiSocialSkype /></div>
-              </div>
+              <h6 className="fw-bold coloryellow m-0 mt-3 p-0">Web Development</h6>
+              <p className="p-0 m-0 mt-3 parasize">DevsThunder is a forward-thinking web development company and software house. We specialize in creating cutting-edge digital solutions that empower businesses and individuals.</p>
+            
               <div className="footer_lineCol1"></div>
-              <p className=" p-0 m-0 mt-3">GrowMe copyright 2023</p>
-              <p className="m-0 p-0">All Rights Reserved | Terms</p>
-              <p className="m-0 p-0 parasizesmall mt-2">All other registered trademarks are property of their respective owners.</p>
+              <p className=" p-0 m-0 mt-3 parasize">DevsThunder copyright 2023</p>
+              <p className="m-0 p-0 parasize">All Rights Reserved | Terms</p>
             </div>
           </div>
           {/* ---------------col2---------------- */}
           <div className="col footer_col2 col-12  col-md-6 col-xl-3 border-0 mt-5 mt-lg-3 border-warning d-flex justify-content-center align-items-center">
-            <ul className="footer_sec_2 minheight border-0 border-danger m-0 text-center">
+          <ul className="footer_sec_2 minheight border-0 border-danger m-0 text-center">
             <li><h6 className="fw-bold coloryellow">Our Services</h6></li>
-              <li className="parasize mt-3">AptEase Company Ledger</li>
-              <li className="parasize  mt-2">AptEase : Ledger Download</li>
+              <li className="parasize  mt-3">Web Design</li>
+              <li className="parasize mt-2">Web Developing</li>
+              <li className="parasize mt-2">Website Maintenance</li>
+              <li className="parasize mt-2">Delivering Websites</li>
             </ul>
+           
           </div>
           {/* ---------------col3---------------- */}
 
           <div className="col footer_col3 col-12 col-md-6 col-xl-3 border-0 mt-5 mt-lg-3 border-warning d-flex justify-content-center align-items-center">
-            <ul className="footer_sec_3 minheight m-0 border-0 border-light m-0 text-center">
+          <ul className="footer_sec_3 minheight m-0 border-0 border-light m-0 text-center">
               <li className="parasize"><h6 className="fw-bold coloryellow">Our Social Networks</h6></li>
               <li className="parasize mt-3">Follow us on Social media and stay up-to-date with all our news and events</li>
-              <li className="parasize  mt-2">(604)210-1553</li>
+              <li className="mt-3"><div className="footer_icons-div m-auto mt-2">
+               <a href="https://www.facebook.com/profile.php?id=61553260912453" target="_blank"> <div className="footer_icon"><LiaFacebookF /></div></a>
+               <a href="https://www.linkedin.com/in/mehwish-tahir-43a378299/" target="_blank">  <div className="footer_icon"><BiLogoLinkedin/></div></a>
+               <a href="https://twitter.com/devs_thunder " target="_blank">  <div className="footer_icon"><BiLogoTwitter /></div></a>
+              </div></li>
             </ul>
           </div>
           {/* ---------------col4---------------- */}
           <div className="col footer_col4 col-12 col-md-6 col-xl-3 border-0 border-warning   mt-5 mt-lg-3 d-flex justify-content-center align-items-center">
             <ul className="footer_sec_4 minheight border-0 border-light text-center">
               <li className="parasize"><h6 className="fw-bold coloryellow">Contact Us</h6></li>
-              <li className="parasize mt-3">+(00)887766 0098</li>
-              <li className="parasize  mt-2">hello@futuremarket.com</li>
-              <li className="parasize  mt-2">One Nell Road Singapore</li>
-
+              <li className="p-0 m-0 mt-3 parasize"><LiaPhoneSolid className="fs-5"/>&nbsp;&nbsp;923013639966</li>
+              <li className="p-0 m-0 mt-2 parasize"><LiaEnvelopeOpenSolid className="fs-5"/>&nbsp;&nbsp;devsthunder@gmail.com</li>
+              <li className="p-0 m-0 mt-2 parasize"><LiaWhatsapp className="fs-5"/>&nbsp;&nbsp;923013639966</li>
+<li className="p-0 m-0 mt-2 parasize"><GoLocation/>&nbsp;&nbsp;239-2-C1 township, lahore</li>
             </ul>
           </div>
           {/* -------------------------------------- */}
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,44 +1,44 @@
 import './header.css';
 import { LiaPhoneSolid } from "react-icons/lia";
-import { FaTwitterSquare } from "react-icons/fa";
 import { LiaEnvelopeOpenSolid } from "react-icons/lia";
 import { LiaFacebookF } from "react-icons/lia";
 import { BiLogoLinkedin } from "react-icons/bi";
 import { BiLogoTwitter } from "react-icons/bi";
 
 import { Link } from 'react-router-dom';
-
 import logo from "./image/WhatsApp_Image_2023-10-25_at_12.09.10_8e72b0d7-removebg-preview.png"
-
 function Header() {
   return (
     <div className='container-fluid p-0 m-0'>
     {/*=============== header element =============*/}
     <div className='mainheader p-0 container-lg'>
-      <header className='w-100 ps-4 text-light d-flex align-items-center justify-content-between' > 
+      <header className='w-100 ps-sm-4 text-light d-flex flex-wrap align-items-center justify-content-between m-0' > 
 
       <div className='header_infodiv d-flex flex-wrap align-items-center p-0 m-0'>
 
           <div className='header_info1 p-0 m-0 d-flex align-items-center'>
             <LiaEnvelopeOpenSolid className='icon'/>
-            <p className='p-0 m-0 ms-1 ms-sm-2'>Chairman@gmail.com</p>
+            <p className='p-0 m-0 ms-1 ms-sm-2'>devsthunder@gmail.com</p>
           </div>
-          <div className='header_info2 m-0 ms-1 ms-sm-3 d-flex align-items-center '>
+          <div className='header_info2 m-0 ms-1 ms-sm-3 d-none d-sm-flex align-items-center '>
             <LiaPhoneSolid className='icon'/>
-            <p  className='p-0 m-0 ms-1 ms-sm-2'>123456789</p>
+            <p  className='p-0 m-0 ms-1 ms-sm-2'>+923013639966</p>
           </div>
         </div>
-        <div className='header_sideDiv h-100 px-sm-5  d-flex'>
+        <div className='header_sideDiv px-md-5 px-sm-2 px-0  d-flex'>
           <div className='header_icondiv fw-bolder d-flex align-items-center justify-content-around px-sm-0 px-4'>
-            <div className="header_icon-outline">
+          <a href="https://www.facebook.com/profile.php?id=61553260912453" target="_blank"> <div className="header_icon-outline">
             <LiaFacebookF />
             </div>
+            </a>
+            <a href="https://www.linkedin.com/in/mehwish-tahir-43a378299/" target="_blank">
             <div className="header_icon-outline">
             <BiLogoLinkedin />
-            </div>
+            </div></a>
+            <a href="https://twitter.com/devs_thunder " target="_blank">
              <div className="header_icon-outline">
              <BiLogoTwitter />
-            </div>
+            </div></a>
           </div>
         </div>
 
