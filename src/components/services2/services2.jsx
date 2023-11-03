1d9f76dfc10624e625c8b94e23bb957ed84e2938
MariumShahid18 <145197996+MariumShahid18@users.noreply.github.com>
2023-11-03 21:07:29 +0500

    services2

--- src/components/services2/services2.jsx
+++ src/components/services2/services2.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
-
+import "./services2.css"
 const Services2 = () => {
-  return (
-    <div>S</div>
-  )
+    return (
+        <>
+            <div className="container-fluid container-lg p-lg-5  services2-banner border-0 border-danger ">
+                <div className="row services2-row1  h-auto border-0 border-success ">
+
+
+                    <div className=" services2-col12 col-10 col-sm-6 col-lg-4 m-auto mt-5 ">
+                        <div className="services2-col1  ">
+                            <span className="services2-col1-number " style={{ color: "#C9AEF8" }}>01</span>
+                            <div className="benefits-of-services2__block ">
+                                <h3 class="benefits-of-services2__title">Powerful front-end</h3>
+                                <p class="benefits-of-services2__text">Our professional website development services company makes sure your solution is wrapped into a performant interface with fast load times and engaging UX design. We balance the UI and business logic to deliver user-friendly experiences.</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="services2-col12 col-10 col-sm-6 col-lg-4 m-auto mt-5">
+                    <div className="col services2-col1   ">
+                        <span className="services2-col1-number " style={{ color: "#EEAACB" }}>02</span>
+                        <div className="benefits-of-services2__block ">
+                            <h3 class="benefits-of-services2__title">Solid back-end</h3>
+                            <p class="benefits-of-services2__text">Scalable architecture is what makes a winning web solution. Our back-end artisans will build a flexible API-friendly back-end with advanced data structures and elastic computing capacity.</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="services2-col12 col-10 col-sm-6 col-lg-4  mt-sm-5 m-auto mt-5 ">
+                    <div className="col services2-col1  ">
+                        <span className="services2-col1-number " style={{ color: "#F9C3B1" }}>03</span>
+                        <div className="benefits-of-services2__block ">
+                            <h3 class="benefits-of-services2__title">Technical support</h3>
+                            <p class="benefits-of-services2__text">As part of our end-to-end professional website development services, we take care of your digital assets, so that they maintain uninterrupted operation. Whether it’s new features or updated security, our developers make sure you have it.</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="services2-col12 col-10 col-sm-6 m-auto mt-5 col-lg-4">
+                    <div className="col services2-col1   ">
+                        <span className="services2-col1-number " style={{ color: "#B6D4B0" }}>04</span>
+                        <div className="benefits-of-services2__block ">
+                            <h3 class="benefits-of-services2__title">Diverse expertise</h3>
+                            <p class="benefits-of-services2__text">As a custom web development services company, we offer a pool of technologies to deliver supreme web assets. Besides fundamental technologies like Java and PHP, we can power your solution with innovative tech stacks.</p>
+                        </div>
+                    </div>
+                    </div>
+
+
+                    <div className="services2-col12 col-10 col-sm-6 m-auto col-lg-4 mt-5">
+                    <div className="col services2-col1  ">
+                        <span className="services2-col1-number " style={{ color: "#A9EEEC" }}>05</span>
+                        <div className="benefits-of-services2__block ">
+                            <h3 class="benefits-of-services2__title">Domain know-how</h3>
+                            <p class="benefits-of-services2__text">Being a global tech partner, we have gained extensive experience in various domains. Be it fintech, martech, or manufacturing, we can tackle your industry challenges with performant web solutions and address specific business needs.</p>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="services2-col12 col-10 col-sm-6 m-auto mt-5  col-lg-4">
+                    <div className="col services2-col1   ">
+                        <span className="services2-col1-number " style={{ color: "#AACFEA" }}>06</span>
+                        <div className="benefits-of-services2__block ">
+                            <h3 class="benefits-of-services2__title">Security</h3>
+                            <p class="benefits-of-services2__text">We prioritize security as one of the pillars of successful digital transformation. That’s why we develop with security in mind, paying attention to authentication and authorization services, access control, and others.</p>
+                        </div>
+                    </div>
+                    </div>
+
+
+
+
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Services2
