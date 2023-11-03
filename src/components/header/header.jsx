c729fd3e6b6fbda2e93b0f83af9259636af94f96
Jaweria Naveed <jawerian09@gmail.com>
2023-11-03 10:32:24 -0700

    website update2

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -59,11 +59,7 @@ function Header() {
     <div className="navbar-nav border-0 ">
       <Link className='nav-link' to="/">Home</Link>
       <Link className='nav-link' to="/services">Services</Link>
-<<<<<<< Updated upstream
       <Link className='nav-link' to="/testimonials">Testimonials</Link>
-=======
-      {/* <Link className='nav-link' to="/pricing">Pricing</Link> */}
->>>>>>> Stashed changes
       <Link className='nav-link' to="/portfolio">Portfolio</Link>
       <Link className='nav-link' to="/about">About</Link>
       <Link className='nav-link' to="/contact">Contact</Link>
