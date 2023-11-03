98b85b2acd53d5b8e5b93391fd9419a93760e469
Jaweria Naveed <jawerian09@gmail.com>
2023-11-03 14:16:00 -0700

    final2

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 import logo from "./image/WhatsApp_Image_2023-10-25_at_12.09.10_8e72b0d7-removebg-preview.png"
 function Header() {
   return (
-    <div className='container-fluid p-0 m-0'>
+    <div className='container-fluid w-100 p-0 m-0'>
     {/*=============== header element =============*/}
     <div className='mainheader p-0 container-lg'>
       <header className='w-100 ps-sm-4 text-light d-flex flex-wrap align-items-center justify-content-between m-0' > 
@@ -25,7 +25,7 @@ function Header() {
             <p  className='p-0 m-0 ms-1 ms-sm-2'>+923013639966</p>
           </div>
         </div>
-        <div className='header_sideDiv px-md-5 px-sm-2 px-0  d-flex'>
+        <div className='header_sideDiv px-md-5 px-sm-2 px-0 d-flex'>
           <div className='header_icondiv fw-bolder d-flex align-items-center justify-content-around px-sm-0 px-4'>
           <a href="https://www.facebook.com/profile.php?id=61553260912453" target="_blank"> <div className="header_icon-outline">
             <LiaFacebookF />
@@ -41,7 +41,6 @@ function Header() {
             </div></a>
           </div>
         </div>
-
       </header>
 
       {/* --------navbar-------- */}
