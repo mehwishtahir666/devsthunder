188f4f1f677d9bbb7f496bf51af327cac7196710
harisalih <147698965+harisalih@users.noreply.github.com>
2023-11-06 13:56:51 +0500

    newsletter

--- src/components/newsletter/newsletter.jsx
+++ src/components/newsletter/newsletter.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./newsletter.css"
 
 const Newsletter = () => {
   return (
-    <div className='p-5 bg-info'>Newsletter...</div>
+    <div className='newsletter'>
+      <div className='text-box'>
+        <h3>Join Our Newsletter</h3>
+        <p>Get fresh news and updates delivered straight to your inbox by subscribing to<br/> our newsletter!</p>
+      </div>
+      <div className="inp-box">
+        <div className="inp-btn">
+          <input type="text" className='news-input' /> 
+          <button>Subscribe</button>
+        </div> 
+      </div>
+    </div> 
   )
 }
 
