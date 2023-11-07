fa08e556e1f5cf6a68c1eb568b81822a4d8707f7
mehwishtahir666 <mehwishtahir666@gmail.com>
2023-11-07 22:58:56 +0500

    next story

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import Banner from './banner/banner'
 import Platforms from './platforms/platforms'
-import Results from './results/results'
 import Services2 from "./services2/services2"
 import Engangment from "./engangment/Engangment"
-import Spliter from './spliter/spliter'
 import Form from "./form/form"
 import Faqs from './faqs/faqs'
 import Newsletter from './newsletter/newsletter'
 import Industries from './industries/industries'
+import Nextstory from './nextstory/nextstory'
 
 
 const Home = () => {
   return (
     <div>
       <Banner/>
-      <Engangment/>
       <Industries/>
+      <Engangment/>
       <Services2/>
       <Newsletter/>
       <Platforms/>
       <Faqs/>
+      <Nextstory/>
       <Form/>
     </div>
   )
