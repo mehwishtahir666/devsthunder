03ca747623c9d2497775dccce0eee6df5e8c0eda
Jaweria Naveed <jawerian09@gmail.com>
2023-11-16 01:18:44 -1200

    code

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,8 +12,8 @@ function Header() {
     <div className='container-fluid w-100 p-0 m-0'>
     {/*=============== header element =============*/}
     <div className='mainheader p-0 container-lg'>
-      <header className='w-100 ps-sm-4 text-light d-flex flex-wrap align-items-center justify-content-between m-0' > 
-      <div className='header_infodiv d-flex flex-wrap align-items-center p-0 m-0'>
+      <header className='w-100 ps-sm-4 ps-2  text-light  m-0' > 
+      <div className='header_infodiv border-0 border-warning d-flex align-items-center p-0 m-0'>
 
           <div className='header_info1 p-0 m-0 d-flex align-items-center'>
             <LiaEnvelopeOpenSolid className='icon'/>
@@ -24,8 +24,8 @@ function Header() {
             <p  className='p-0 m-0 ms-1 ms-sm-2'>+923013639966</p>
           </div>
         </div>
-        <div className='header_sideDiv px-md-5 px-sm-2 px-0 d-flex'>
-          <div className='header_icondiv fw-bolder d-flex align-items-center justify-content-around px-sm-0 px-4'>
+        <div className='header_sideDiv border-0 border-danger px-sm-4 px-3 d-flex'>
+          <div className='header_icondiv fw-bolder d-flex align-items-center justify-content-around px-sm-0'>
           <a href="https://www.facebook.com/profile.php?id=61553260912453" target="_blank"> <div className="header_icon-outline">
             <LiaFacebookF />
             </div>
@@ -52,8 +52,8 @@ function Header() {
   <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav border-0 ">
+  <div className="collapse p-0 m-0 navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
       <Link className='nav-link' to="/">Home</Link>
       <Link className='nav-link' to="/services">Services</Link>
       <Link className='nav-link' to="/testimonials">Testimonials</Link>
